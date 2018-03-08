c47b9b650508ce15dcfa3ee50131aaf6b9fa8a7e
Alexander Austin <austinag@g.cofc.edu>
2018-03-08 18:38:20 -0500

    URL connection

--- app/src/main/java/com/example/alex/csci_490_lab_04/MainActivity.java
+++ app/src/main/java/com/example/alex/csci_490_lab_04/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.alex.csci_490_lab_04;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +20,34 @@ public class MainActivity extends AppCompatActivity {
         WebView webview = new WebView(this);
         setContentView(webview);
         webview.loadUrl("https://www.wikipedia.org");
+        String url = getUsers("https://jsonplaceholder.typicode.com/users");
+        Log.i("Test URl", url);
+
+    }
+
+    public String getUsers(String URL)  {
+        URL source;
+        try {
+            source = new URL(URL);
+            URLConnection connection = source.openConnection();
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String retLine;
+            StringBuilder sBuild = new StringBuilder();
+            while ((retLine = br.readLine()) != null) {
+                sBuild.append(retLine);
+            }
+
+            Log.i("Test URl", sBuild.toString());
+            return sBuild.toString();
+
+
+        } catch (Exception e) {
+            Log.i("Error", e.getMessage());
+
+        }
+
+        return "oops";
+
+
     }
 }
